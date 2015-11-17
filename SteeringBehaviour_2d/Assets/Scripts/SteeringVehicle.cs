8cf190417c8524d02eb8e0b9572dbfd2501b5c16
Berend Weij <info@berendweij.nl>
2015-11-17 15:14:07 +0100

    added Unity C# steering behaviour

--- SteeringBehaviour_2d/Assets/Scripts/SteeringVehicle.cs
+++ SteeringBehaviour_2d/Assets/Scripts/SteeringVehicle.cs
@@ -12,19 +12,10 @@ public class SteeringVehicle : MonoBehaviour {
 
 	private Rigidbody2D rigidBody;
 	private Vector2 target;
-
-	private int[,] numbers;
-
 	
 	// Use this for initialization
 	void Start () {
 		rigidBody = GetComponent<Rigidbody2D>();
-
-		numbers = new int[10,10];
-		numbers[1,1] = 5;
-		numbers[3,2] = 10;
-
-		Debug.Log ("numbers[1]: " + numbers[1]);
 	}
 	
 	// Update is called once per frame
@@ -39,7 +30,7 @@ public class SteeringVehicle : MonoBehaviour {
 	void Seek () {
 
 		// we berekenen eerst de afstand/Vector tot de 'target' (in dit voorbeeld de andere cubus)		
-		Vector2 desiredStep	=	target - rigidBody.position;
+		Vector2 desiredStep	= target - rigidBody.position;
 
 		// deze desiredStep mag niet groter zijn dan de maximale Speed
 		//
@@ -50,15 +41,15 @@ public class SteeringVehicle : MonoBehaviour {
 		// als je deze weer vermenigvuldigt met de maximale snelheid dan
 		// wordt de lengte van deze Vector maxSpeed (aangezien 1 x maxSpeed = maxSpeed)
 		// de x en y van deze Vector wordt zo vanzelf omgerekend
-		Vector2 desiredVelocity			=	desiredStep	*	maxSpeed;
+		Vector2 desiredVelocity = desiredStep * maxSpeed;
 		
 		// bereken wat de Vector moet zijn om bij te sturen om bij de desiredVelocity te komen
-		Vector2 steeringForce			=	desiredVelocity - rigidBody.velocity;
+		Vector2 steeringForce = desiredVelocity - rigidBody.velocity;
 
 		// uiteindelijk voegen we de steering force toe maar wel gedeeld door de 'mass'
 		// hierdoor gaat hij niet in een rechte lijn naar de target
 		// hoe zwaarder het object hoe moeilijker hij kan bijsturen
-		rigidBody.velocity				=	rigidBody.velocity + steeringForce / mass;
+		rigidBody.velocity = rigidBody.velocity + steeringForce / mass;
 
 		// @ code templates laten zien
 
